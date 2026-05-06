7dba0ed0ec832a5b8c24217f3b26dacdb7b4d248
Deva-97 <devendiran03@gmail.com>
2026-05-06 17:53:19 +0530

    folders created

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Insurance Reminders',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
