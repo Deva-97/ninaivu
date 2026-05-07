2213a4db4dc24e901e7649fb5cf5efde6bf1ee27
Deva-97 <devendiran03@gmail.com>
2026-05-07 13:25:12 +0530

    firebase addition done

--- android/app/src/main/kotlin/com/devendiran/insurance_reminders/MainActivity.kt
+++ android/app/src/main/kotlin/com/devendiran/insurance_reminders/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.insurance_reminders
+package com.devendiran.insurance_reminders
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class InsuranceRemindersApp extends StatelessWidget {
+  const InsuranceRemindersApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Insurance Reminders",
+      // initialBinding: Binding(),
+      // initialRoute: AppRoutes.splashScreen,
+      // getPages: AppPages.pages,
+      // theme: AppTheme.light,
+    );
+  }
+}
